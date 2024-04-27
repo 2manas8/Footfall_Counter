f642751b173abc8d8567f58d441038b4ac3fab6b
Manas Pratim Das <114216265+2manas8@users.noreply.github.com>
2024-04-27 22:03:38 +0530

    Added WiFi check and screen timeout functionalities

--- Arduino_Uno/src/main.cpp
+++ Arduino_Uno/src/main.cpp
@@ -2,16 +2,23 @@
 #include <TFT.h>  
 #include <SPI.h>
 
-#define cs 10
-#define dc 9
-#define rst 8
-
 int count = 0;
 
+const int cs = 10;
+const int dc = 9;
+const int rst = 8;
 TFT lcd = TFT(cs, dc, rst);
-int buzzer = 2;
-int led = 3;
-int ldr = 4;
+bool reset = 0;
+
+const int buzzer = 2;
+const int led = 3;
+const int ldr = 4;
+const int esp = 5;
+const int updating = 6;
+const int wifiConnection = 7;
+
+unsigned long previousMillis = 0;
+const int interval = 5000;
 
 int xAxis() {
   if((count >= 0) && (count <= 9)) {
@@ -25,32 +32,77 @@ int xAxis() {
 }
 
 void greet() {
-  lcd.background(255, 255, 255);
+  lcd.fillScreen(0xFFFF);
+  reset = 1;
   lcd.text("Welcome", 42, 40);
   lcd.text("dear", 60, 60);
   lcd.text("customer!", 30, 80);
 }
 
+void wifiCheck() {
+  lcd.fillScreen(0xFFFF);
+  reset = 0;
+  lcd.text("Connecting", 27, 50);
+  int temp = 68;
+  while(digitalRead(wifiConnection) == LOW) {
+    if(temp > 82) {
+      lcd.fillRect(0, 70, 160, 20, 0xFFFF);
+      temp = 68;
+    }
+    lcd.text(".", temp, 70);
+    temp += 7;
+    delay(1000);
+  }
+}
+
 void setup() {
-  pinMode(buzzer, OUTPUT);
-  pinMode(led, OUTPUT);
-  digitalWrite(led, HIGH);
-  pinMode(ldr, INPUT);
+  // pinMode(buzzer, OUTPUT);
+  // pinMode(led, OUTPUT);
+  // digitalWrite(led, HIGH);
+  // pinMode(ldr, INPUT);
+  // pinMode(esp, OUTPUT);
+  // digitalWrite(esp, LOW);
+  // pinMode(updating, INPUT);
+  pinMode(wifiConnection, INPUT);
   lcd.begin();
   lcd.stroke(0, 0, 0);
   lcd.setTextSize(2);
+  if(digitalRead(wifiConnection) == LOW) {
+    wifiCheck();
+  }
   greet();
 }
 
 void loop() {
-  if(digitalRead(ldr) == HIGH) {
-    count++;
-    lcd.background(255, 255, 255);
-    lcd.setTextSize(1);
-    lcd.text("Current Footfall:", 30, 52);
-    lcd.setTextSize(2);
-    lcd.text(String(count).c_str(), xAxis(), 68);
-    delay(5000);
+  if(digitalRead(wifiConnection) == LOW) {
+    wifiCheck();
+  }
+  unsigned long currentMillis = millis();
+  // if(digitalRead(ldr) == HIGH) {
+  //   previousMillis = currentMillis;
+  //   reset = 0;
+  //   count++;
+  //   lcd.fillScreen(0xFFFF);
+  //   lcd.setTextSize(1);
+  //   lcd.text("Current Footfall:", 35, 52);
+  //   lcd.setTextSize(2);
+  //   lcd.text(String(count).c_str(), xAxis(), 68);
+  //   digitalWrite(buzzer, HIGH);
+  //   delay(100);
+  //   digitalWrite(buzzer, LOW);
+  //   digitalWrite(esp, HIGH);
+  //   delay(100);
+  //   digitalWrite(esp, LOW);
+  //   while(digitalRead(updating) == HIGH) {
+  //     if(currentMillis - previousMillis >= interval) {
+  //       previousMillis = currentMillis;
+  //       greet();
+  //     }
+  //   }
+  // }
+  if((currentMillis - previousMillis >= interval) && (!reset)) {
+    previousMillis = currentMillis;
+    reset = 1;
     greet();
   }
 }
