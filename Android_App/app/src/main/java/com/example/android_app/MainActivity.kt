980d1fcb37895e166224fde217ee27943b488d62
Manas Pratim Das <dasmp123@gmail.com>
2024-04-30 23:44:14 +0530

    Made changes to the UI and app icon

--- Android_App/app/src/main/java/com/example/android_app/MainActivity.kt
+++ Android_App/app/src/main/java/com/example/android_app/MainActivity.kt
@@ -1,15 +1,11 @@
-@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
-
 package com.example.android_app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.rememberScrollableState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,19 +17,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -64,7 +55,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun FootfallData() {
-    var offset by remember { mutableFloatStateOf(0f) }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -79,7 +69,7 @@ fun FootfallData() {
         ) {
             Column {
                 Image(
-                    painter = painterResource(id = R.drawable.icon_image_circle),
+                    painter = painterResource(id = R.drawable.mall_icon_circle),
                     contentDescription = null,
                     modifier = Modifier.size(60.dp)
                 )
@@ -95,38 +85,37 @@ fun FootfallData() {
             }
         }
         Spacer(modifier = Modifier.padding(25.dp))
-        TextField(
-            value = stringResource(id = R.string.footfall_data),
-            onValueChange = {},
-            colors = TextFieldDefaults.textFieldColors(
-                containerColor = Color.LightGray,
-                unfocusedTextColor = Color.Black,
-                unfocusedIndicatorColor = Color.Black
-            ),
-            readOnly = true,
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
             modifier = Modifier
                 .align(alignment = Alignment.CenterHorizontally)
                 .height(530.dp)
                 .width(330.dp)
-                .background(color = Color.Transparent)
-                .scrollable(
-                    orientation = Orientation.Horizontal,
-                    state = rememberScrollableState { delta ->
-                        offset += delta
-                        delta
-                    }
-                )
-        )
+                .background(color = Color.LightGray)
+                .verticalScroll(state = ScrollState(0))
+        ) {
+            Text(
+                text = stringResource(id = R.string.footfall_data),
+                color = Color.Black,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp)
+            )
+        }
         Spacer(modifier = Modifier.padding(20.dp))
         Row(
+            verticalAlignment = Alignment.Bottom,
             horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(50.dp)
         ) {
             Button(
                 onClick = { /* TODO */ },
                 shape = RoundedCornerShape(10.dp),
                 colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0xFF4874B6),
+                    containerColor = Color(0xFF0052D0),
                     contentColor = Color.White
                 )
             ) {
